d6139d87c1464bc4ae8d4de2906fc338d999dc7e
Keerthan Ekbote <saiskee@gmail.com>
2026-09-24 00:35:41 -0400

    Improve Riyaz mobile layout

--- riyaaz/sw.js
+++ riyaaz/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "riyaaz-v2";
+const CACHE = "riyaaz-v3";
 const SHELL = ["/riyaaz/", "/riyaaz/manifest.webmanifest", "/riyaaz/favicon.svg"];
 
 self.addEventListener("install", event => {
